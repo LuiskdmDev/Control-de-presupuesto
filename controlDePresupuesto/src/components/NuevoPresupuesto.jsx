6802029727a434601a433d3c0f6826f8541afdc6
LuiskdmDev <nta.luisedcaroca@gmail.com>
2024-01-10 19:27:23 -0300

    hola2

--- controlDePresupuesto/src/components/NuevoPresupuesto.jsx
+++ controlDePresupuesto/src/components/NuevoPresupuesto.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import {useState} from 'react';
+import mensaje from './Mensaje.jsx';
+
 
 const NuevoPresupuesto = ({ presupuesto, setPresupuesto}) => {
+
+    const [mensaje, setMensaje] = useState('')
+
+    //Handle es el nombre que usualmente se le colocan a las funciones que administran
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+
+        if(!Number(presupuesto) || Number (presupuesto) < 0) {
+            setMensaje("No es un presupuesto válido")
+            
+        } else {
+            console.log("Es una cifra válida")
+        }
+
+    }
+
   return (
     <section className='contenedor-presuuesto contenedor sombra'>
-        <form className='formulario'>
+        <form onSubmit={handlePresupuesto} className='formulario'>
             <div className='campo'>
                 <label> Definir Presupuesto</label>
 
                 <input 
-                lassName='nuevo-presupuesto'
+                className='nuevo-presupuesto'
                 type='text'
                 placeholder='Añade tu Presupuesto'
                 value={presupuesto}
@@ -16,6 +34,8 @@ const NuevoPresupuesto = ({ presupuesto, setPresupuesto}) => {
                 />
 
                 <input type="submit" value="Añadir" />
+
+                {mensaje && <mensaje tipo="error"> {mensaje} </mensaje>}
             </div>
             
         </form>
